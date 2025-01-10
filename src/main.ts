0df0ab1110354382a0a6795eff21d21b8a1bf641
Fenix Quiroz <fenixquiroz10@gmail.com>
2025-01-09 20:22:44 -0500

    tarea terminada

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,57 @@
-import "./style.css";
+let turno: number = 1;
+let numeroTurno = document.getElementById("numero-turno");
+const turnoAnterior = document.getElementById("turno-anterior");
+const botonReset = document.getElementById("reset");
+const turnoSiguiente = document.getElementById("turno-siguiente");
 
-console.log("Hello Typescript!");
+const actualizarContador = (): void => {
+  if (numeroTurno instanceof HTMLHeadingElement) {
+    numeroTurno.textContent = turno.toString().padStart(2, "0");
+    if (turnoAnterior instanceof HTMLButtonElement) {
+      turnoAnterior.disabled = turno <= 1;
+    }
+  }
+};
+
+const inputIngresarTurno = document.getElementById("ingresar-turno") as HTMLInputElement;
+const botonIngresarTurno = document.getElementById("boton-ingresar-turno");
+
+const handlerIngresarTurno = (): void => {
+  turno = parseInt(inputIngresarTurno.value);
+  inputIngresarTurno.value = '' 
+  
+    actualizarContador();
+};
+
+if (botonIngresarTurno instanceof HTMLButtonElement) {
+  botonIngresarTurno.addEventListener("click", handlerIngresarTurno);
+}
+
+const handlerTurnoAnterior = (): void => {
+  if (numeroTurno instanceof HTMLHeadingElement) {
+    if (turno > 1) {
+      turno--;
+      actualizarContador();
+    }
+  }
+};
+const reset = (): void => {
+  turno = 0;
+  actualizarContador();
+};
+const handleTurnoSiguiente = (): void => {
+  if (numeroTurno instanceof HTMLHeadingElement) {
+    turno++;
+    actualizarContador();
+  }
+};
+
+if (turnoAnterior !== null && turnoAnterior !== undefined) {
+  turnoAnterior.addEventListener("click", handlerTurnoAnterior);
+}
+if (botonReset !== null && botonReset !== undefined) {
+  botonReset.addEventListener("click", reset);
+}
+if (turnoSiguiente !== null && turnoSiguiente !== undefined) {
+  turnoSiguiente.addEventListener("click", handleTurnoSiguiente);
+}
